771426e7bfd83b9c533a9628be51b6919258373d
Krisna Pranav <krisna.pranav@gmail.com>
2022-01-24 12:58:38 +0530

    types: [vbytes + sub + default for freeRate]

--- src/types.rs
+++ src/types.rs
@@ -4,8 +4,9 @@ use std::ops::Sub;
 use bitcoin::blockdata::transaction::{OutPoint, Transaction, TxOut};
 use bitcoin::{hash_types::Txid, util::psbt};
 use serde::{Deserialize, Serialize};
+use sled::LogKind::Free;
 
-#[derive(Serialze, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub enum KeychainKind {
     External = 0,
     Internal = 1,
@@ -27,4 +28,24 @@ impl AsRef<[u8]> for KeychainKind {
             KeychainKind::Internal => b"i",
         }
     }
+}
+
+pub struct FreeRate(f32);
+
+impl std::default::Default for FreeRate {
+    fn default() -> Self {
+        unimplemented!()
+    }
+}
+
+impl Sub for FreeRate {
+    type Output = Self;
+
+    fn sub(self, other: FreeRate) -> Self::Output {
+        FreeRate(self.0 - other.0)
+    }
+}
+
+pub trait vBytes {
+    fn vbytes(self) -> usize;
 }
